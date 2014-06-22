409bab16f3ec54298fc203e8c011c54b8ec0c3bb
Tajinder <tajindersingh4487@gmail.com>
2014-06-21 21:58:32 -0500

    Signed-off-by: Tajinder <tajindersingh4487@gmail.com>

--- src/main/java/com/app/orderbuzz/dto/ResourcesDto.java
+++ src/main/java/com/app/orderbuzz/dto/ResourcesDto.java
@@ -10,13 +10,16 @@ public class ResourcesDto {
 	}
 	public void setRest_id_pk(BigInteger rest_id_pk) {
 		this.rest_id_pk = rest_id_pk;
+		
 	}
+	// url
 	public String getRest_photo() {
 		return rest_photo;
 	}
 	public void setRest_photo(String rest_photo) {
 		this.rest_photo = rest_photo;
 	}
+	
 	
 	
 }
